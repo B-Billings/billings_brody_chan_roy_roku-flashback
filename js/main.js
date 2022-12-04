871ae83ba353bbbf1c6c2d64fc4a0bce049b3c9d
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2022-12-03 22:52:07 -0500

    finished with the base files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,57 @@
 (() => {
+    const { createApp } = Vue
 
-})();
+    createApp({
+        created() {
+            console.log('vue instance is created');
+            //fetch('./scripts/json2.php')
+            fetch('./data.json')
+                .then(res => res.json())
+                .then(data => this.ProjectData = data)
+            .catch(error => console.error(error));
+        },
+
+        data() {
+            console.log('vue data check');
+            return {
+                ProjectData: {},
+            }
+        },
+        
+        components: {
+        }
+    }).mount('#app')
+
+    createApp({
+        created() {
+            console.log('vue instance is created');
+        },
+
+        data() {
+            console.log('vue data check');
+            return {
+                ProjectData: {},
+            }
+        },
+        
+        components: {
+        }
+    }).mount('#sidebar')
+
+    createApp({
+        created() {
+            console.log('vue instance is created');
+        },
+
+        data() {
+            console.log('vue data check');
+            return {
+                ProjectData: {},
+            }
+        },
+        
+        components: {
+        }
+    }).mount('#profile')
+    
+})()
